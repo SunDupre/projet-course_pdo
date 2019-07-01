20336d5f06242a3a4695aec4b67aefea60440297
SunDupre <dupre.mcse@gmail.com>
2019-07-01 13:20:51 +0200

    Créate db et table Mysql

---
+++ initialscripte.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE AnnuaireToutou;
+USE AnnuaireToutou;
+CREATE USER "AdminToutou" @"localhost" IDENTIFIED BY "Annu@ireT0ut0u",
+GRANT ALL PRIVILEGES ON AnnuaireToutou.* TO "AdminToutou" @"localhost",
+FLUSH PRIVILEGES,
+CREATE TABLE Maitres (
+  id int primary key AUTO_INCREMENT,
+  nom VARCHAR(200),
+  telephone VARCHAR(20),
+) 
+CREATE TABLE Chiens (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nom VARCHAR(255),
+  age INT,
+  race VARCHAR(200),
+  id_maitre INT,
+  FOREIGN KEY (id-maitre) REFERENCES Maitres(id),
+)
